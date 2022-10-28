f327dfecc777ad865e96e2054fcac3a08645b278
John Gibson <jhngbsn09@gmail.com>
2022-10-28 10:55:10 -0400

    Created initial objects for the game

---
+++ script.js
@@ -0,0 +1,12 @@
+// module to stoe the gameboard
+let gameBoard = (function() {
+    let _board = [[0,1,2],[3,4,5],[6,7,8]];
+    
+
+})();
+
+
+// factory function to make the player
+const Player = (name, symbol) => {
+
+}
