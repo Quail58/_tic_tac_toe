eae16938e06574f4a785fc38f441e22658df65bd
John Gibson <jhngbsn09@gmail.com>
2022-11-02 12:27:23 -0400

    Created a for statement to have empty cells

--- script.js
+++ script.js
@@ -2,23 +2,45 @@
 const gameBoard = (function(){
     //matrix for the board
     let _board = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8]
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
     ];
     const grid = document.getElementById('board');
 
+    //makes a grid for the gameboard
     const makeGrid = () => {       
         for (i = 0; i < 9; i++){
-            cell = document.createElement("div");
+            cell = document.createElement("button");
             cell.setAttribute('id', 'tacbox');
+            // checks for the first array
             if (i <=2 ){
-                cell.setAttribute('class', _board[0][i]);
+                //sets to empty if its not "X" or "O"
+                if (_board[0][i] == 0) {
+                    cell.innterHTML= ""
+                } else {
+                    cell.setAttribute('class', _board[0][i]);
+                    cell.innerHTML = _board[0][i];
+                }
+            // checks for the second array
             } else if (i >=3 && i < 6){
-                cell.setAttribute('class', _board[1][i-3]);
+                //sets to empty if its not "X" or "O"
+                if (_board[1][i-3] == 0) {
+                    cell.innterHTML= ""
+                } else {
+                    cell.setAttribute('class', _board[1][i-3]);
+                    cell.innerHTML = _board[1][i-3];
+                }
+            // defaults to the third array                
             } else {
-                cell.setAttribute('class', _board[2][i-6]);
-            }       
+                //sets to empty if its not "X" or "O"
+                if (_board[2][i-6] == 0) {
+                    cell.innterHTML= ""
+                } else {
+                    cell.setAttribute('class', _board[2][i-6]);
+                    cell.innerHTML = _board[2][i-6];
+                }  
+            }     
             grid.appendChild(cell);
         } 
     }
@@ -34,3 +56,4 @@ console.log(gameBoard.makeGrid());
 const Player = (name, symbol) => {
 
 }
+
