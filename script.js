03d7c6fa1978d15345e429f7765d2e6ccaa3ba4c
John Gibson <jhngbsn09@gmail.com>
2022-11-04 17:45:09 -0400

    Added reset button When reset button is clicked it resets the array When reset button is clicked it resets board

--- script.js
+++ script.js
@@ -1,50 +1,136 @@
 // module to store the gameboard
 const gameBoard = (() => {
+    let gameOn = true;
     //matrix for the board
     let _board = [
         [1,2,3],
         [4,5,6],
         [7,8,9]
     ];
+
+
+
     //creates the grid
     const _grid = document.getElementById('middle');
+    const _bottom = document.getElementById('bottom');
     let _counter = 0;
+    let playCount = 0;
+    //function to create win screen
+    function _winScreen(token){
+        const winTitle = document.createElement('div');
+        winTitle.setAttribute('class', 'win');
+        winTitle.innerHTML = token
+        _bottom.appendChild(winTitle);
+        const resetButton = document.createElement('button');
+        resetButton.innerHTML = "Play Again?";
+        resetButton.onclick = function() {
+            while(_grid.firstChild){
+                _grid.removeChild(_grid.lastChild);
+              }
+              gameOn = true;
+              
+              _board[0].splice(0, 3, 1, 2, 3);
+              _board[1].splice(0, 3, 4, 5, 6);
+              _board[2].splice(0, 3, 7, 8, 9);
+              makeGrid();
+              resetButton.remove();
+              winTitle.remove();
+              playCount = 0;              
+            }            
+        ;
+        _bottom.appendChild(resetButton);
+    }
+
+    // function clearBoard() {
+    //     while(_grid.firstChild){
+    //       _grid.removeChild(_grid.lastChild);
+    //     }
+    //     makeGrid();
+    //   }
+
+
+    function resetBoard() {
+        deleteBoard = document.getElementById('board');
+        deleteBoard.remove;
+        makeGrid();
+        };
 
     //function to compare the values of the array
     function _checkWin() {
-        const _deleteBoard = document.getElementById('board');
         //checks across
         if (_board[0][0] == _board[0][1] && _board[0][0] == _board[0][2]){
             if (_board[0][0] == 'X'){
-                console.log('X wins');
-                _deleteBoard.remove();                
+                _winScreen('X wins!');
+                gameOn = false;                
             } else {
-                console.log('O wins');
-                _deleteBoard.remove();
+                _winScreen('O wins!');
+                gameOn = false;
             }
         } else if(_board[1][0] == _board[1][1] && _board[1][0] == _board[1][2]) {
-            console.log("win!");
+            if (_board[1][0] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
 
         } else if(_board[2][0] == _board[2][1] && _board[2][0] == _board[2][2]) {
-            console.log("win!");
+            if (_board[2][0] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
 
         } else if(_board[0][0] == _board[1][0] && _board[0][0] == _board[2][0]) {
-            console.log("win!");
+            if (_board[0][0] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
 
         } else if(_board[0][1] == _board[1][1] && _board[0][1] == _board[2][1]){
-            console.log("win!");
+            if (_board[0][1] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
 
         } else if(_board[0][2] == _board[1][2] && _board[0][2] == _board[2][2]){
-            console.log("win!");
+            if (_board[0][2] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
 
         }else if(_board[0][0] == _board[1][1] && _board[0][0] == _board[2][2]) {
-            console.log("win!");
+            if (_board[0][0] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
             
         } else if(_board[0][2] == _board[1][1] && _board[0][2] == _board[2][0]){
-            console.log("win!");
+            if (_board[0][2] == 'X'){
+                _winScreen('X wins!');
+                gameOn = false;                
+            } else {
+                _winScreen('O wins!');
+                gameOn = false;
+            }
 
-        } else if(_counter == 9){
-            console.log("Its a tie!")
+        } else if(playCount == 9){
+            _winScreen("It's a tie!")
         }
     }
 
@@ -58,6 +144,8 @@ const gameBoard = (() => {
             cell.setAttribute('class', i);
             cell.setAttribute('id', 'gridChild')
             cell.addEventListener('click', function changeSymbol() {
+            //runs the game if its playing
+            if(gameOn == true){
                 //replaces token slot with "X"
                 if (_counter % 2 == 0){
                     tokenSet = this.className;
@@ -66,7 +154,7 @@ const gameBoard = (() => {
                     this.innerHTML = 'X';
                     this.disabled = true;
                     _counter++;
-                    console.log(_board);
+                    playCount++;
                     //changes the array
                     if (tokenSet <= 2){ 
                         _board[0].splice(tokenSet, 1, "X");
@@ -86,7 +174,7 @@ const gameBoard = (() => {
                     this.innerHTML = 'O';
                     this.disabled = true;
                     _counter++;
-                    console.log(_board);
+                    playCount++;
                     //changes the array
                     if (tokenSet <= 2){ 
                         _board[0].splice(tokenSet, 1, "O");
@@ -98,7 +186,9 @@ const gameBoard = (() => {
                         _board[2].splice(tokenSetThree, 1, "O");
                         _checkWin();
                     }              
-                }                
+                }
+            //stops the game if its not playing
+            }               
             });     
             createBoard.appendChild(cell);
         } 
