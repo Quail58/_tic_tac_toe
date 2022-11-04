20e8efc4e85b2b2c68dd41fc18cbbd059ce755ec
John Gibson <jhngbsn09@gmail.com>
2022-11-04 13:31:12 -0400

    Finished function to chagne array Moved the win check function

--- script.js
+++ script.js
@@ -2,59 +2,84 @@
 const gameBoard = (() => {
     //matrix for the board
     let _board = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
     ];
     //creates the grid
     const _grid = document.getElementById('board');
     let _counter = 0;
-    const X = "X"
-    const O = "O"
+
+    //function to compare the values of the array
+    function _checkWin() {
+        //checks across
+        if (_board[0][0] == _board[0][1] && _board[0][0] == _board[0][2] || _board[1][0] == _board[1][1] && _board[1][0] == _board[1][2] || _board[2][0] == _board[2][1] && _board[2][0] == _board[2][2]){
+            console.log("win!")
+        
+        //checks up and down
+        } else if(_board[0][0] == _board[1][0] && _board[0][0] == _board[2][0] || _board[0][1] == _board[1][1] && _board[0][1] == _board[2][1] || _board[0][2] == _board[1][2] && _board[0][2] == _board[2][2]) {
+            console.log("win!")
+
+        //checks diagonal
+        } else if(_board[0][0] == _board[1][1] && _board[0][0] == _board[2][2] || _board[0][2] == _board[1][1] && _board[0][2] == _board[2][0]) {
+            console.log("win!")
+        }
+    }
 
     //makes a grid for the gameboard
     function makeGrid(){       
-        for (i = 0; i < 9; i++){           
+        for (i = 0; i < 9; i++){         
             cell = document.createElement("button");
             cell.setAttribute('class', i);
             cell.addEventListener('click', function changeSymbol() {
+
+                //replaces token slot with "X"
                 if (_counter % 2 == 0){
+                    tokenSet = this.className;
+                    tokenSetTwo = this.className - 3;
+                    tokenSetThree = this.className - 6;
                     this.innerHTML = 'X';
                     this.disabled = true;
                     _counter++;
-                    console.log(_board);
-                    console.log()
-                    if (this.className <= 2){
-                        _board[0][this.classname] = 1;
+                    //changes the array
+                    if (tokenSet <= 2){ 
+                        _board[0].splice(tokenSet, 1, "X");
+                        _checkWin();
+                    } else if(tokenSet > 2 && tokenSet < 6) {
+                        _board[1].splice(tokenSetTwo, 1, "X");
+                        _checkWin();                        
+                    } else{
+                        _board[2].splice(tokenSetThree, 1, "X");
+                        _checkWin();
                     }
+
+                //replaces token slot with "0" 
                 } else {
+                    tokenSet = this.className;
+                    tokenSetTwo = this.className - 3;
+                    tokenSetThree = this.className - 6;
                     this.innerHTML = 'O';
                     this.disabled = true;
                     _counter++;
-                    console.log(_board);              
+                    console.log(_board);
+                    console.log(this.className)
+                    //changes the array
+                    if (tokenSet <= 2){ 
+                        _board[0].splice(tokenSet, 1, "O");
+                        _checkWin();
+                    } else if(tokenSet > 2 && tokenSet < 6) {
+                        _board[1].splice(tokenSetTwo, 1, "O");
+                        _checkWin();                        
+                    } else{
+                        _board[2].splice(tokenSetThree, 1, "O");
+                        _checkWin();
+                    }              
                 }                
             });     
             _grid.appendChild(cell);
         } 
     }
     
-    //function to compare the values of the array
-    function _checkWin() {
-        //checks across
-        if (_board[0][0] == _board[0][1] && _board[0][0] == _board[0][2] || _board[1][0] == _board[1][1] && _board[1][0] == _board[1][2] || _board[2][0] == _board[2][1] && _board[2][0] == _board[2][2]){
-
-        
-        //checks up and down
-        } else if(_board[0][0] == _board[1][0] && _board[0][0] == _board[2][0] || _board[0][1] == _board[1][1] && _board[0][1] == _board[2][1] || _board[0][2] == _board[1][2] && _board[0][2] == _board[2][2]) {
-
-
-        //checks diagonal
-        } else if(_board[0][0] == _board[1][1] && _board[0][0] == _board[2][2] || _board[0][2] == _board[1][1] && _board[0][2] == _board[2][0]) {
-
-
-        }
-    }
-
     return {
         makeGrid
     }    
